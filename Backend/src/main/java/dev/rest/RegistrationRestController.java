f937b8c363853e107d78fa032aa9d10dd4df372b
Rhyme Rubayet <rubayet211@gmail.com>
2024-01-02 07:56:16 +0600

    Phase-2 Sign In & Register

---
+++ Backend/src/main/java/dev/rest/RegistrationRestController.java
@@ -0,0 +1,29 @@
+package dev.rest;
+
+import dev.domain.RegistrationRequest;
+import dev.domain.User;
+import dev.domain.UserDetail;
+import dev.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class RegistrationRestController {
+    private final RegistrationService registrationService;
+
+    @Autowired
+    public RegistrationRestController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @PostMapping("/register")
+    public String  registerUser(@RequestBody RegistrationRequest registrationRequest) {
+
+        User user = registrationRequest.getUser();
+        UserDetail userDetail = registrationRequest.getUserDetail();
+
+        registrationService.registerUser(user, userDetail);
+
+        return "User registered successfully";
+    }
+}
---
+++ Backend/src/main/java/dev/service/RegistrationService.java
@@ -0,0 +1,33 @@
+package dev.service;
+
+import dev.domain.User;
+import dev.domain.UserDetail;
+import dev.repository.UserDetailRepository;
+import dev.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@Transactional
+public class RegistrationService {
+
+    private final UserRepository userRepository;
+    private final UserDetailRepository userDetailRepository;
+
+    @Autowired
+    public RegistrationService(UserRepository userRepository, UserDetailRepository userDetailRepository) {
+        this.userRepository = userRepository;
+        this.userDetailRepository = userDetailRepository;
+    }
+
+    public void registerUser(User user, UserDetail userDetail) {
+
+        userRepository.create(user);
+        userDetail.setUser(user);
+        userDetailRepository.createDetailDetail(userDetail);
+    }
+}
